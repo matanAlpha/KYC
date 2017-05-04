ceeba96ef6655f87f291822f9c008821bf763c72
admin <admin@admins-imac.elcam.co.il>
2017-05-04 05:42:13 +0300

    Initial Commit

---
+++ KYC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KYC
+//
+//  Created by admin on 8 Iyar 5777.
+//  Copyright © 5777 Matan alpha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
